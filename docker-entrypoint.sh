ded76479373bbeb6bc5c807c955b0397bc15985a
mia <viashipcosmos@gmail.com>
2025-10-26 14:42:35 +0800

    fix: remove MySQL readiness check from docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,4 @@
-until mysql -h smai_db -uappuser -pCoS_fa_au_23mi -e "select 1" tstDB; do
-  echo "Waiting for MySQL to be ready..."
-  sleep 2
-done
+
 export PYTHONPATH=/app
 # Check if there are any model changes before creating a revision
 if ! conda run -n app alembic -c /app/config/alembic.ini check | grep -q "No new upgrade operations found"; then
